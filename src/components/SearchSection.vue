d062b651c56f35ad884ecf2506d608a6012d4ea1
wtfgn <exal59@yahoo.com>
2023-08-27 16:47:14 +0800

    update

---
+++ src/components/SearchSection.vue
@@ -0,0 +1,12 @@
+<template>
+	<p>Search Section</p>
+	<button class="temp" @click="search">Search</button>
+</template>
+
+<script setup lang="ts">
+const search = () => {
+	console.log('search');
+};
+</script>
+
+<style scoped></style>
