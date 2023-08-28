cee1b51ed0d679c5f374ffd1a54c2b9cb5466caa
wtfgn <exal59@yahoo.com>
2023-08-28 17:07:25 +0800

    Add Filter Template

---
+++ src/components/FilterTemplate.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="filter">
+    <div class="filter-title">
+      <slot name="title" /> <!-- This is where the title will go -->
+    </div>
+    <div class="filter-container">
+      <slot name="filter" /> <!-- This is where the filter will go -->
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+
+</script>
+
+<style scoped></style>
