7ef43cf7693447f3d7d107aabe4627e690e5b25e
wtfgn <exal59@yahoo.com>
2023-08-29 20:51:41 +0800

    Use suspense for async

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,17 @@
 <template>
-  <SearchSection />
+  <Suspense>
+    <template #default>
+      <SearchSection />
+    </template>
+
+    <template #fallback>
+      <div>Loading...</div>
+    </template>
+  </Suspense>
 </template>
 
 <script setup lang="ts">
-import SearchSection from './components/SearchSection.vue';
+import SearchSection from '@/components/SearchSection.vue';
 </script>
 
 <style scoped></style>
