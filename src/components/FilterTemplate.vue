5f87e964aabf1b5ba61718e3a776f2ef112f099e
wtfgn <exal59@yahoo.com>
2023-08-31 15:04:06 +0800

    Apply css

--- src/components/FilterTemplate.vue
+++ src/components/FilterTemplate.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="filter">
-    <div class="filter-title">
+  <div class="filter flex flex-col mb-4">
+    <div class="filter-title flex flex-row justify-center text-center rounded-md bg-slate-300">
       <slot name="title" /> <!-- This is where the title will go -->
     </div>
-    <div class="filter-container">
+    <div class="filter-container flex flex-row">
       <slot name="filter" /> <!-- This is where the filter will go -->
     </div>
   </div>
@@ -12,27 +12,4 @@
 <script setup lang="ts">
 </script>
 
-<style scoped>
-.filter {
-  display: flex;
-  flex-direction: column;
-  margin-bottom: 1rem;
-}
-
-.filter-title {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background-color: #f5f5f5;
-  border: 1px solid #e0e0e0;
-  border-radius: 0.25rem;
-  padding: 0.5rem;
-  color: #616161;
-}
-
-.filter-container {
-  display: flex;
-  flex-direction: column;
-  padding: 0.5rem;
-}
-</style>
+<style scoped></style>
