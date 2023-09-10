3b696ddd0fb5ce085acc7fddbeaf0d072bd4dc29
wtfgn <exal59@yahoo.com>
2023-09-10 15:43:47 +0800

    Add tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: (theme) => ({
+        'repeat-gradient-background':
+          'linear-gradient(90deg,rgba(0,2,4,.95) 0,rgba(23,53,75,0) 50.14%,rgba(0,2,4,.95) 100%),url(/src/assets/background/bg_common-repeat.png)',
+      }),
+    },
+    plugins: [],
   },
-  plugins: [],
-}
+};
